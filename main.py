3dbe3746026f5585370e65f46684424f94a2a35e
mumfy27-rgb <mumfy27@gmail.com>
2026-04-29 16:25:21 +0930

    Add player rendering and triangle shape

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
+
+
 
 def main():
     pygame.init()
 
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
@@ -20,7 +29,12 @@ def main():
             
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
+        
       
     
      
