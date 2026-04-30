7bca193f2bccb7acc7fa2a4446ed68e05640e5f5
mumfy27-rgb <mumfy27@gmail.com>
2026-04-30 17:01:31 +0930

    feat: replace player with spaceship sprite

--- player.py
+++ player.py
@@ -16,27 +16,20 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.shoot_timer = 0 
+        self.shoot_timer = 0
+
+        # 🚀 Load spaceship image
+        self.image = pygame.image.load("ship.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))
 
     def shoot(self):
         if self.shoot_timer > 0:
             return
 
         shot = Shot(self.position.x, self.position.y)
-        
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
         self.shoot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
-
-    def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-
-        return [a, b, c]
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -44,6 +37,7 @@ class Player(CircleShape):
     def update(self, dt):
         if self.shoot_timer > 0:
             self.shoot_timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -63,4 +57,7 @@ class Player(CircleShape):
         self.position += direction * PLAYER_SPEED * dt
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, (0, 200, 255), self.triangle(), LINE_WIDTH)
+        # 🔄 Rotate and draw the ship
+        rotated_image = pygame.transform.rotate(self.image, -self.rotation)
+        rect = rotated_image.get_rect(center=self.position)
+        screen.blit(rotated_image, rect)
